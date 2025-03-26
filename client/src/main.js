f71bb7563c3df0f19be2ab3d42a2ae0be3202183
Kacper Nalepa <kacper.nlp@wp.pl>
2025-03-27 00:25:54 +0100

    feat: setup app

---
+++ client/src/main.js
@@ -0,0 +1,21 @@
+import "./assets/main.css";
+
+import { createApp } from "vue";
+import PrimeVue from "primevue/config";
+import Aura from "@primeuix/themes/aura";
+
+import App from "./App.vue";
+
+import Button from "primevue/button";
+
+const app = createApp(App);
+
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+});
+
+app.component("Button", Button);
+
+app.mount("#app");
